2b7e92575c18e301ce86f39a5243f2f6eae2c42a
Seyda Koclar <seydakoclar@gmail.com>
2022-05-13 16:09:43 +0200

    Form1 implementation added

--- ActionsWithAgents/Form1.Designer.cs
+++ ActionsWithAgents/Form1.Designer.cs
@@ -29,92 +29,101 @@ namespace ActionsWithAgents
         /// </summary>
         private void InitializeComponent()
         {
-            this.statements = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.actions = new System.Windows.Forms.Label();
             this.agents = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.fluents = new System.Windows.Forms.Label();
+            this.statements = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // actions
+            // 
+            this.actions.AutoSize = true;
+            this.actions.Location = new System.Drawing.Point(58, 91);
+            this.actions.Name = "actions";
+            this.actions.Size = new System.Drawing.Size(54, 17);
+            this.actions.TabIndex = 0;
+            this.actions.Text = "Actions";
+            // 
+            // agents
+            // 
+            this.agents.AutoSize = true;
+            this.agents.Location = new System.Drawing.Point(58, 165);
+            this.agents.Name = "agents";
+            this.agents.Size = new System.Drawing.Size(52, 17);
+            this.agents.TabIndex = 1;
+            this.agents.Text = "Agents";
+            // 
+            // fluents
+            // 
+            this.fluents.AutoSize = true;
+            this.fluents.Location = new System.Drawing.Point(58, 243);
+            this.fluents.Name = "fluents";
+            this.fluents.Size = new System.Drawing.Size(54, 17);
+            this.fluents.TabIndex = 2;
+            this.fluents.Text = "Fluents";
             // 
             // statements
             // 
-            this.statements.Location = new System.Drawing.Point(664, 318);
+            this.statements.Location = new System.Drawing.Point(647, 329);
             this.statements.Name = "statements";
-            this.statements.Size = new System.Drawing.Size(97, 30);
-            this.statements.TabIndex = 0;
+            this.statements.Size = new System.Drawing.Size(102, 40);
+            this.statements.TabIndex = 3;
             this.statements.Text = "Statements";
             this.statements.UseVisualStyleBackColor = true;
             this.statements.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(123, 91);
+            this.textBox2.Location = new System.Drawing.Point(126, 162);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(638, 22);
-            this.textBox2.TabIndex = 2;
+            this.textBox2.Size = new System.Drawing.Size(623, 22);
+            this.textBox2.TabIndex = 5;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // actions
-            // 
-            this.actions.AutoSize = true;
-            this.actions.Location = new System.Drawing.Point(46, 91);
-            this.actions.Name = "actions";
-            this.actions.Size = new System.Drawing.Size(49, 20);
-            this.actions.TabIndex = 4;
-            this.actions.Text = "Actions";
-            this.actions.UseCompatibleTextRendering = true;
-            this.actions.Click += new System.EventHandler(this.initially_Click);
-            // 
-            // agents
-            // 
-            this.agents.AutoSize = true;
-            this.agents.Location = new System.Drawing.Point(43, 157);
-            this.agents.Name = "agents";
-            this.agents.Size = new System.Drawing.Size(52, 17);
-            this.agents.TabIndex = 6;
-            this.agents.Text = "Agents";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(123, 154);
+            this.textBox3.Location = new System.Drawing.Point(126, 240);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(638, 22);
-            this.textBox3.TabIndex = 8;
-            // 
-            // fluents
-            // 
-            this.fluents.AutoSize = true;
-            this.fluents.Location = new System.Drawing.Point(43, 221);
-            this.fluents.Name = "fluents";
-            this.fluents.Size = new System.Drawing.Size(54, 17);
-            this.fluents.TabIndex = 9;
-            this.fluents.Text = "Fluents";
-            this.fluents.Click += new System.EventHandler(this.label1_Click_2);
+            this.textBox3.Size = new System.Drawing.Size(623, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(123, 218);
+            this.textBox1.Location = new System.Drawing.Point(126, 88);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(638, 22);
-            this.textBox1.TabIndex = 10;
+            this.textBox1.Size = new System.Drawing.Size(623, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Please enter each values with comma seperated";
+            // 
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.fluents);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.agents);
-            this.Controls.Add(this.actions);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.statements);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Controls.Add(this.fluents);
+            this.Controls.Add(this.agents);
+            this.Controls.Add(this.actions);
+            this.Name = "Form2";
+            this.Text = "Actions with Agents";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,13 +131,13 @@ namespace ActionsWithAgents
 
         #endregion
 
-        private System.Windows.Forms.Button statements;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label actions;
         private System.Windows.Forms.Label agents;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label fluents;
+        private System.Windows.Forms.Button statements;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
-
--- ActionsWithAgents/Form1.cs
+++ ActionsWithAgents/Form1.cs
@@ -15,45 +15,56 @@ namespace ActionsWithAgents
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void initially_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_2(object sender, EventArgs e)
-        {
-
-        }
-
+        public string agentText;
+        public string actionText;
+        public string fluentText;
         private void button1_Click(object sender, EventArgs e)
         {
+            string[] agentNames = agentText.Split(',');
+            string[] actionNames=  actionText.Split(',');
+            string[] fluentNames = fluentText.Split(',');
 
+            Agent[] agents = new Agent[agentNames.Length];
+            Fluent[] fluents = new Fluent[fluentNames.Length];
+            Action[] actions = new Action[actionNames.Length];
+
+            int i = 0;
+            foreach (string agentName in agentNames)
+            {
+                agents[i] = new Agent(agentName);
+                i++;
+            }
+            i = 0;
+
+            foreach (string fluentName in fluentNames)
+            {
+                fluents[i] = new Fluent(fluentName);
+                i++;
+            }
+          
+            i = 0;
+            foreach (string actionName in actionNames)
+            {
+                actions[i] = new Action(actionName);
+                i++;
+            }
+            // Form2 frm2 = new Form2();
+            //frm2.Show();
         }
 
-        private void signature_Click(object sender, EventArgs e)
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
-
+            fluentText = textBox3.Text;
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
+            agentText = textBox2.Text;
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            actionText = textBox1.Text;
         }
     }
 }
