cb458804ba56f3dacc1c255f271812088dea6716
Seyda Koclar <seydakoclar@gmail.com>
2022-05-24 22:09:31 +0200

    Transition and State classes are added

---
+++ ActionsWithAgents/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActionsWithAgents
+{
+    // This class is for storing transitions as objects having list of fluents as property and
+    // one constructor for creating a transition with two states, one agent and one action. This
+    // will be used for simulating graph of the system and then used for executing queries.
+    class Transition
+    {
+        Agent agent;
+        Action action;
+        State starting;
+        State resulting;
+
+        public Transition(Agent ag, Action ac, State s, State r)
+        {
+            agent = ag;
+            action = ac;
+            starting = s;
+            resulting = r;
+        }
+    }
+}
---
+++ ActionsWithAgents/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActionsWithAgents
+{
+    // This class is for storing states as objects having list of fluents as property and
+    // one constructor for creating a state with the list of fluents
+    public class State
+    {
+        List<Fluent> fluents;
+        public State(List<Fluent> _fluents)
+        {
+            fluents = _fluents;
+        }
+    }
+}
