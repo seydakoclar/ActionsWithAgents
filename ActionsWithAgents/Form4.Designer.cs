dfab55ccdee60343f1facef7266116a0bc149f87
Seyda Koclar <seydakoclar@gmail.com>
2022-06-09 13:57:31 +0200

    bug ficing on inconsistency

--- ActionsWithAgents/Form4.Designer.cs
+++ ActionsWithAgents/Form4.Designer.cs
@@ -42,6 +42,7 @@ namespace ActionsWithAgents
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -161,11 +162,22 @@ namespace ActionsWithAgents
             this.checkBox5.Text = "~";
             this.checkBox5.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(680, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 52;
+            this.button3.Text = "NEW";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.checkBox5);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
@@ -202,5 +214,6 @@ namespace ActionsWithAgents
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button3;
     }
 }
