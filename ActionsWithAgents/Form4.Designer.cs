5fc6ec23705b7b37c52d5bd0400a115459c510cc
Seyda Koclar <seydakoclar@gmail.com>
2022-06-09 11:13:24 +0200

    query execution added

--- ActionsWithAgents/Form4.Designer.cs
+++ ActionsWithAgents/Form4.Designer.cs
@@ -41,13 +41,14 @@ namespace ActionsWithAgents
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(164, 75);
+            this.label1.Location = new System.Drawing.Point(188, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 17);
             this.label1.TabIndex = 0;
@@ -56,7 +57,7 @@ namespace ActionsWithAgents
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 52);
+            this.label2.Location = new System.Drawing.Point(28, 53);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 17);
             this.label2.TabIndex = 1;
@@ -65,17 +66,16 @@ namespace ActionsWithAgents
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(37, 72);
+            this.comboBox1.Location = new System.Drawing.Point(75, 73);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(107, 24);
             this.comboBox1.TabIndex = 2;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(275, 73);
+            this.textBox1.Location = new System.Drawing.Point(299, 73);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(480, 22);
+            this.textBox1.Size = new System.Drawing.Size(456, 22);
             this.textBox1.TabIndex = 3;
             // 
             // button1
@@ -96,6 +96,7 @@ namespace ActionsWithAgents
             this.button2.TabIndex = 9;
             this.button2.Text = "EXECUTE QUERY";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
@@ -111,7 +112,6 @@ namespace ActionsWithAgents
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 24);
             this.comboBox2.TabIndex = 7;
-            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // label3
             // 
@@ -151,11 +151,22 @@ namespace ActionsWithAgents
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.List;
             // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(31, 75);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(38, 21);
+            this.checkBox5.TabIndex = 51;
+            this.checkBox5.Text = "~";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox5);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
@@ -190,5 +201,6 @@ namespace ActionsWithAgents
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.CheckBox checkBox5;
     }
 }
--- ActionsWithAgents/Form4.cs
+++ ActionsWithAgents/Form4.cs
@@ -16,15 +16,25 @@ namespace ActionsWithAgents
         List<Fluent> fluents;
         List<Transition> transitions;
         List<Graph> graphs;
-        public Form4(List<Agent> a, List<Fluent> f, List<Transition> t, List<Graph> g)
+        List<Statement> statements;
+        List<Action> actions;
+        List<Tuple<Action, Agent>> programSequence;
+
+        public Form4(List<Agent> a, List<Fluent> f, List<Action> ac, List<Transition> t,List<Statement> s, List<Graph> g)
         {
+            actions = ac;
             agents = a;
             fluents = f;
             transitions = t;
+            statements = s;
             graphs = g;
             InitializeComponent();
         }
 
+        bool checkInconsistencyOfStateHasNoTransitionFunction()
+        {
+            return false;
+        }
         private void Form4_Load(object sender, EventArgs e)
         {
             foreach (Agent a in agents)
@@ -36,23 +46,127 @@ namespace ActionsWithAgents
                 comboBox1.Items.Add(a.Name);
             }
 
-            //Check inconsistency
+            if(checkInconsistencyOfStateHasNoTransitionFunction())
+            {
+
+            }
+            else
+            {
+
+            }
 
         }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
+            //holds after
+            foreach(ListViewItem i in listView1.Items)
+            {
+                listView1.Items.Remove(i);
+            }
+            if(comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Required fields of the query must be chosen");
+            }
+            else
+            {
+                //(prepare,Anna),(eat,alex)
+                Fluent f = new Fluent(fluents[comboBox1.SelectedIndex]);
+                f.ChangeInit(!checkBox5.Checked);
+                string[] strArr = textBox1.Text.Split(',');
+                programSequence = new List<Tuple<Action, Agent>> { };
+                for(int i=0; i<strArr.Length; i+=2)
+                {
+                    Action a = new Action(strArr[i].Substring(1));
+                    Agent ag = new Agent(strArr[i+1].Substring(0, strArr[i + 1].Length - 1));
+                    programSequence.Add(new Tuple<Action, Agent>(a, ag));
+                }
+                if(graphs.Count == 1)
+                {
+                    State resultingState = graphs[0].initialState;
+                    foreach (Tuple<Action, Agent> pair in programSequence)
+                    {
+                        Transition t = transitions.Find(delegate (Transition f1)
+                        {
+                            return f1.action.Name == pair.Item1.Name && f1.agent.Name == pair.Item2.Name && f1.starting.Name == resultingState.Name;
+                        });
+                        resultingState = t.resulting;
+                    }
+                    MessageBox.Show("  " + resultingState.Name + " ");
+                    Fluent lastFluent = resultingState.fluents.Find(delegate (Fluent f1)
+                    {
+                        return f1.Name == f.Name;
+                    });
+                    if(lastFluent.Initial == f.Initial)
+                    {
+                        listView1.Items.Add("TRUE");
+                    }
+                    else
+                    {
+                        listView1.Items.Add("FALSE");
+                    }
+                }
+                else
+                {
+                    //implement
+                }
+                
+            }
+        }
 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //involves
+            foreach (ListViewItem i in listView1.Items)
+            {
+                listView1.Items.Remove(i);
+            }
+            if (comboBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show("Required fields of the query must be chosen");
+            }
+            else
+            {
+                Agent agent = agents[comboBox2.SelectedIndex];
+                bool isInvolved = false;
+                string[] strArr = textBox2.Text.Split(',');
+                programSequence = new List<Tuple<Action, Agent>> { };
+                for (int i = 0; i < strArr.Length; i += 2)
+                {
+                    Action a = new Action(strArr[i].Substring(1));
+                    Agent ag = new Agent(strArr[i + 1].Substring(0, strArr[i + 1].Length - 1));
+                    programSequence.Add(new Tuple<Action, Agent>(a, ag));
+                }
+
+                if (graphs.Count == 1)
+                {
+                    State resultingState = graphs[0].initialState;
+                    foreach (Tuple<Action, Agent> pair in programSequence)
+                    {
+                        Transition t = transitions.Find(delegate (Transition f1)
+                        {
+                            return f1.action.Name == pair.Item1.Name && f1.agent.Name == pair.Item2.Name && f1.starting.Name == resultingState.Name;
+                        });
+                        if (resultingState.Name != t.resulting.Name && t.agent.Name == agent.Name)
+                            isInvolved = true;
+                        resultingState = t.resulting;
+                    }
+                   
+                    if (isInvolved)
+                    {
+                        listView1.Items.Add("TRUE");
+                    }
+                    else
+                    {
+                        listView1.Items.Add("FALSE");
+                    }
+                }
+                else
+                {
+                    //implement
+                }
+            }
         }
     }
 }
